6c756777978ef71daffb539858d0f70a4667764c
Gurlivleen Singh Kainth <gurlivleen.kainth2000@gmail.com>
2021-03-09 16:25:03 +0530

    Importing Bootstrap to Express App

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => res.send('Hello User'));
-app.get('/home', (req, res) => res.send('Welcome to Home Page'))
+// Importing static files to express app
+app.use(express.static(path.join(__dirname, "/node_modules/bootstrap/dist")));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
 app.get('/contactus', (req, res) => res.send('Welcome to Contact Us Page'))
 app.get('/about', (req, res) => res.send('Welcome to About Page'))
 
 app.get('/product', (req, res) => res.send('Welcome to Products Page'))
 app.get('/product/:id', (req, res) => {
     res.send('Welcome to Product Detailed Page');
-    res.send(`Seeing Details of ${req.params.id}`);
 })
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
