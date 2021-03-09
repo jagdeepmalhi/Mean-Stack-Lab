fadc05548d523a7dbe2cc3b0a7b001c7678ab253
Gurlivleen Singh Kainth <gurlivleen.kainth2000@gmail.com>
2021-03-09 16:04:11 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => res.send('Hello User'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
